a6aaeee86f0a44a4a934b8a68bfcf3e5a3bde9da
pressbutt <benni.acker@gmail.com>
2017-09-30 21:47:03 +0200

    ich habe alles gefixt!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var mymap;
 var baseLayers;
-
+var Overlays;
 navigator.geolocation.getCurrentPosition(function(location) {
 
     console.log(location.coords.latitude);
@@ -9,26 +9,28 @@ navigator.geolocation.getCurrentPosition(function(location) {
 
     console.log(location.coords.accuracy);
 
-    mymap = L.map('mapid').setView([location.coords.latitude, location.coords.longitude], 13);
+
 
     baseLayers = {
 
-        "Straßenkarte": L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        "Strassenkarte": L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            maxZoom: 22,
-        }).addTo(mymap)
+
+        })
     };
 
-    overlaysMap = {
-        "Regen": L.tileOverlay('http://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=2d3c8b0f8243563d61a2c6628a873f59', {
+    Overlays = {
+        "Regen": L.tileLayer('http://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=2d3c8b0f8243563d61a2c6628a873f59', {
             attribution: ' © <a href=\'https://www.mapbox.com/about/maps/\'>Mapbox</a> © <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a> <strong><a href=\'https://www.mapbox.com/map-feedback/\' target=\'_blank\'>Improve this map</a></strong>',
-            maxZoom: 22,
+
         }),
-        "Wind":L.tileOveray('http://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=2d3c8b0f8243563d61a2c6628a873f59', {
+        "Wind":L.tileLayer('http://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=2d3c8b0f8243563d61a2c6628a873f59', {
             attribution: ' © <a href=\'https://www.mapbox.com/about/maps/\'>Mapbox</a> © <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a> <strong><a href=\'https://www.mapbox.com/map-feedback/\' target=\'_blank\'>Improve this map</a></strong>',
-            maxZoom: 22,
+
         }),
-    }
+    };
+
+    mymap = L.map('mapid',{layers:[baseLayers.Strassenkarte]}).setView([location.coords.latitude, location.coords.longitude], 13);
 
     L.control.layers(baseLayers, Overlays).addTo(mymap);
     //L.control.layers(Overlays, {}).addTo(mymap);
