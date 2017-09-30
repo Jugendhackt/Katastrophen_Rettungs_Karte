7804c4ff374ff9723eddfd7837c00b347fcf0cb9
Gregor Freudenberger <freudekrecks@gmail.com>
2017-09-30 16:16:53 +0200

    GPS Daten eingebunden Grundschtrucktur verbessert

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap;
 
+navigator.geolocation.getCurrentPosition(function(location) {
 
-L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-}).addTo(mymap);
+    console.log(location.coords.latitude);
 
-function onMapClick(e) {
+    console.log(location.coords.longitude);
+
+    console.log(location.coords.accuracy);
+
+    mymap = L.map('mapid').setView([location.coords.latitude, location.coords.longitude], 13);
+
+    L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+    }).addTo(mymap);
+
+    function onMapClick(e) {
 //    alert("You clicked the map at " + e.latlng);
-    var marker = L.marker(e.latlng).addTo(mymap);
-}
+        var marker = L.marker(e.latlng).addTo(mymap);
+    }
 
-mymap.on('click', onMapClick);
+    mymap.on('click', onMapClick);
+
+});
