3b78254b991c8b9d327e6b406281de6e997a574a
Gregor Freudenberger <freudekrecks@gmail.com>
2017-09-30 21:10:02 +0200

    Filter dazu fügen, Bilder für buttons

--- js/main.js
+++ js/main.js
@@ -12,20 +12,30 @@ navigator.geolocation.getCurrentPosition(function(location) {
     mymap = L.map('mapid').setView([location.coords.latitude, location.coords.longitude], 13);
 
     baseLayers = {
-        "Wetterkarte": L.tileLayer('http://{s}.tiles.mapbox.com/v3/openstreetmap.map-4wvf9l0l/{z}/{x}/{y}.png', {
-            attribution: ' © <a href=\'https://www.mapbox.com/about/maps/\'>Mapbox</a> © <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a> <strong><a href=\'https://www.mapbox.com/map-feedback/\' target=\'_blank\'>Improve this map</a></strong>',
-            maxZoom: 18,
-        }),
+
         "Straßenkarte": L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            maxZoom: 18,
+            maxZoom: 22,
         }).addTo(mymap)
     };
-    L.control.layers(baseLayers, {}).addTo(mymap);
+
+    overlaysMap = {
+        "Regen": L.tileOverlay('http://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=2d3c8b0f8243563d61a2c6628a873f59', {
+            attribution: ' © <a href=\'https://www.mapbox.com/about/maps/\'>Mapbox</a> © <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a> <strong><a href=\'https://www.mapbox.com/map-feedback/\' target=\'_blank\'>Improve this map</a></strong>',
+            maxZoom: 22,
+        }),
+        "Wind":L.tileOveray('http://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=2d3c8b0f8243563d61a2c6628a873f59', {
+            attribution: ' © <a href=\'https://www.mapbox.com/about/maps/\'>Mapbox</a> © <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a> <strong><a href=\'https://www.mapbox.com/map-feedback/\' target=\'_blank\'>Improve this map</a></strong>',
+            maxZoom: 22,
+        }),
+    }
+
+    L.control.layers(baseLayers, Overlays).addTo(mymap);
+    //L.control.layers(Overlays, {}).addTo(mymap);
 
     function onMapClick(e) {
 //    alert("You clicked the map at " + e.latlng);
-        var marker = L.marker(e.latlng).addTo(mymap);
+        var marker1 = L.marker1(e.latlng).addTo(mymap);
     }
 
     mymap.on('click', onMapClick);
