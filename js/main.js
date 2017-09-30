9e010bed4526f4799df69cd988c6e6cdc9283aee
Gregor Freudenberger <freudekrecks@gmail.com>
2017-09-30 18:05:55 +0200

    Grundstrucktur fertig Wetterkarte eingebunden, aber noch nicht eingebunden

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var mymap;
+var baseLayers;
 
 navigator.geolocation.getCurrentPosition(function(location) {
 
@@ -10,10 +11,17 @@ navigator.geolocation.getCurrentPosition(function(location) {
 
     mymap = L.map('mapid').setView([location.coords.latitude, location.coords.longitude], 13);
 
-    L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
-    }).addTo(mymap);
+    baseLayers = {
+        "Wetterkarte": L.tileLayer('http://{s}.tiles.mapbox.com/v3/openstreetmap.map-4wvf9l0l/{z}/{x}/{y}.png', {
+            attribution: ' © <a href=\'https://www.mapbox.com/about/maps/\'>Mapbox</a> © <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a> <strong><a href=\'https://www.mapbox.com/map-feedback/\' target=\'_blank\'>Improve this map</a></strong>',
+            maxZoom: 18,
+        }),
+        "Straßenkarte": L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+        }).addTo(mymap)
+    };
+    L.control.layers(baseLayers, {}).addTo(mymap);
 
     function onMapClick(e) {
 //    alert("You clicked the map at " + e.latlng);
